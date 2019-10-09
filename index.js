337722894f05480d3437e3ed0e6295119fa50c92
FIAP <FIAP>
2019-10-08 22:30:30 -0300

    add route users with GET method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("oi");
+const express = require('express');
+
+const app = express(); // Criando nova instância do express 
+
+// Criando rotas
+app.get('/users', (request, response, next) => {
+    console.log("Entrei na rota User!");
+
+    response.json({success: true})
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
