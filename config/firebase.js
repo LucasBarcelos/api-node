23d19842efb12fd3d9ece98284f836bd5d945e03
Lucas Barcelos <lucas.barcelos@dbccompany.com.br>
2019-10-10 22:50:46 -0300

    Integração com o firebase

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyC0O8jueArsvRUDhBtYqbqY3eGTzP340o8",
+    authDomain: "api-node-barcelos.firebaseapp.com",
+    databaseURL: "https://api-node-barcelos.firebaseio.com",
+    projectId: "api-node-barcelos",
+    storageBucket: "api-node-barcelos.appspot.com",
+    messagingSenderId: "20819820509",
+    appId: "1:20819820509:web:ac1b57dece6c3200164034",
+    measurementId: "G-WC2S0CFVSX"
+};
--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const express = require('express');
 
-const app = express(); // Criando nova instância do express 
+// Firebase
+const firebase = require('firebase');
+const firebaseConfig = require('./config/firebase');
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const app = express(); // Criando nova instância do express
 
 // Criando rotas
-app.get('/users', (request, response, next) => {
-    console.log("Entrei na rota User!");
+app.get('/users/:id', (request, response, next) => {
+    const id = request.params.id;
 
-    response.json({success: true})
+    db.collection('users').doc(id).get()
+        .then(user => {
+            if(!user.exists) {
+                response
+                    .sendstatus(404);
+                    //.send({ message: 'No Content' });
+            }
+
+            response.json(user.data());
+        })
+        .catch(err => {
+            response
+                .sendstatus(500);
+                console.log(err);
+                console.log('Error getting document', err);
+        });
 });
+
+
+app.get('/users', (request, response, next) => {
+// Buscar todos os dados da coleção 'users'
+
+var arrayUsers = [];
+
+db.collection('users').get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      arrayUsers.push(doc.data());
+    });
+    response.json(arrayUsers);
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });
+});
+
 
 const port = process.env.PORT || 3000;
 
